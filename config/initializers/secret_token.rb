48f38f466e5fce5c591ffd603d1dd8955ac24787
malpin <malpinvector@gmail.com>
2015-05-05 06:37:08 +0000

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = 'a6a15badb390d86006b241af94db6d902a0bdcd66014d239424270ae98ed68aba62c7989fc9e32240dbc9ea35ed0fe3590478bb943605d6b5b81d15fc061ac26'
